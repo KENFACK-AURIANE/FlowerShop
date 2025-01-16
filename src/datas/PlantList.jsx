6790200207a2c0190f7f60dfba828a40f1f2da37
KENFACK AURIANE <kenfackauriane2@gmail.com>
2025-01-16 13:29:17 +0100

    ajout de plantes manquantes

--- src/datas/PlantList.jsx
+++ src/datas/PlantList.jsx
@@ -1,43 +1,53 @@
 import monstera from '../assets/monstera.jpg'
-import logo from '../assets/logo.png'
+import lyrata from '../assets/lyrata.jpg'
+import pothos from '../assets/pothos.jpg'
+import succulent from '../assets/succulent.jpg'
+import olivier from '../assets/olivier.jpg'
+import basil from '../assets/basil.jpg'
+import mint from '../assets/mint.jpg'
+import calathea from '../assets/calathea.jpg'
+import cactus from '../assets/cactus.jpg'
 
 export const plantList = [
 	{
 		name: 'monstera',
 		category: 'classique',
 		id: '1ed',
-		isBestSale: true,
+		bestSale: true,
 		light: 2,
-		water: 1,
+		water: 3,
 		cover: monstera,
-		price:8
+		price: 15
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
 		id: '2ab',
 		light: 3,
-		water: 2,
-		cover: monstera,
-		price:5
+		water: 1,
+		cover: lyrata,
+		price: 16
 	},
+
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
 		light: 1,
 		water: 2,
-		cover: monstera,
-		price:10
+		cover: pothos,
+
+		price: 9
 	},
 	{
-		name: 'yucca',
+		name: 'calathea',
 		category: 'classique',
 		id: '4kk',
-		light: 3,
-		water: 1,
-		cover: monstera,
-		price:6
+		light: 2,
+		water: 3,
+		cover: calathea,
+
+		price: 20
 	},
 	{
 		name: 'olivier',
@@ -45,36 +55,28 @@ export const plantList = [
 		id: '5pl',
 		light: 3,
 		water: 1,
-		cover: monstera,
-		price:12
+		cover: olivier,
+		price: 25
 	},
+
 	{
-		name: 'géranium',
-		category: 'extérieur',
-		id: '6uo',
+		name: 'cactus',
+		category: 'plante grasse',
+		id: '8fp',
 		light: 2,
-		water: 2,
-		cover: monstera,
-		price:4
+		water: 1,
+		cover: cactus,
+		price: 6
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
-		isBestSale: true,
+		bestSale: true,
 		light: 2,
 		water: 3,
-		cover: monstera,
-		price:10
-	},
-	{
-		name: 'aloe',
-		category: 'plante grasse',
-		id: '8fp',
-		light: 2,
-		water: 1,
-		cover: logo,
-		price:3
+		cover: basil,
+		price: 5
 	},
 	{
 		name: 'succulente',
@@ -82,7 +84,17 @@ export const plantList = [
 		id: '9vn',
 		light: 2,
 		water: 1,
-		cover: monstera,
-		price:2
+		cover: succulent,
+		price: 8
+	},
+
+	{
+		name: 'menthe',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: mint,
+		price: 4
 	}
 ]
