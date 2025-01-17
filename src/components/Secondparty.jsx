b8d4498ea4dedc07fc197be05b61a3e80d79c11f
KENFACK AURIANE <kenfackauriane2@gmail.com>
2025-01-17 06:29:47 +0100

    creation de la seconde partie de la page

---
+++ src/components/Secondparty.jsx
@@ -0,0 +1,23 @@
+
+const Secondparty = () => {
+  return (
+    <div className="flex flex-col text-center gap-8 pt-16 pb-16">
+      <div className=" flex flex-col gap-2">
+        <h1 className=" font-serif text-3xl text-black sm:text-4xl ">Livraison des plantes au Cameroun</h1>
+        <h4 className=" flex text-center sm:px-10">
+            Nos payements se font une fois à l&apos;arrive et les frais de livraison sont compris dans le prix donné.Nos payements se font une fois à l&apos;arrive et les frais de livraison sont compris dans le prix donné.Nos payements se font une fois à l&apos;arrive et les frais de livraison sont compris dans le prix donné.
+        </h4>
+      </div>
+      <div className=" flex flex-col gap-2">
+        <h1 className=" font-serif text-2xl text-black sm:text-3xl">
+            Nos horaires
+        </h1>
+        <p className=" text-red-600">
+            Nous sommes ouvert tous les jours de 7h à 18h
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Secondparty
